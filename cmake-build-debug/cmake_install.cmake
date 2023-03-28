b8f2ad957abd399830f212a0948e00f86cca7ea6
NodeReplacer <francismlee@hotmail.com>
2023-03-27 21:55:23 -0400

    Hopefully source is self-contained
    Hopefully making it so that you can just build from by unzipping the source code and having it work from the box.
    
    This was done by making cmake look for SFML in the root directory and then just putting the necessary SFML version in that folder. In this case, the 2.5.1 32-bit version.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GravMaybe")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Basic-Game")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
